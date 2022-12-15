13fa96fc067011a08e4f41fe983f8e4ecd721d1c
Rawston <houston_cam@msn.com>
2022-12-15 00:20:15 -0300

    Organized Project!!!

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@ import HelloWorld from "./components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Em Desenvolvimento!!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{name: 'home'}">Home</RouterLink>
+        <RouterLink :to="{name: 'todos.index'}">Todos</RouterLink>
       </nav>
     </div>
   </header>
